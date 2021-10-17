5c59cae560227d888b9d6fa0f1a7c77a9ab6399e
Matheus Reis <mateusrreis5855@hotmail.com>
2021-10-17 16:26:35 -0300

    project initial

--- src/main/java/com/example/ocrreconizer/SecondFragment.kt
+++ src/main/java/com/example/ocrreconizer/SecondFragment.kt
@@ -1,5 +1,4 @@
 package com.example.ocrreconizer
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,21 +7,27 @@ import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.ocrreconizer.databinding.FragmentSecondBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVPrinter
+import java.lang.StringBuilder
 
 
 class SecondFragment : Fragment() {
 
+
     val args: SecondFragmentArgs by navArgs()
     private var binding: FragmentSecondBinding? = null
     private var adapter: ArrayAdapter<String>? = null
+    private var floatingActionButton: FloatingActionButton? =null
 
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?, floatingActionButton: FloatingActionButton,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentSecondBinding.inflate(inflater, container, false);
 
-        binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding?.root
     }
 
@@ -30,10 +35,26 @@ class SecondFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding?.tvStore?.text = args.itemList[0]
         setupAdapter(args.itemList.copyOfRange(1, args.itemList.size))
+        val mFab = view.findViewById<FloatingActionButton>(R.id.extended_fab)
+        mFab.setOnClickListener {
+            buildCsv(args.itemList.copyOfRange(1, args.itemList.size));
+        }
     }
 
     private fun setupAdapter(itens: Array<String>){
         adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, itens)
         binding?.itemList?.adapter = adapter
+    }
+
+    private fun clickListner(){
+
+    }
+
+    private fun buildCsv(itens : Array<String>){
+        val sb = StringBuilder();
+        val csvPrinter = CSVPrinter(sb, CSVFormat.DEFAULT.withHeader("NFiscal","Name_Corporation","cnpjAndCpf"));
+        csvPrinter.printRecord("teste","teste","teste");
+        csvPrinter.flush();
+        csvPrinter.close();
     }
 }
---
+++ src/main/java/com/example/ocrreconizer/models/NfModel.kt
@@ -0,0 +1,7 @@
+package com.example.ocrreconizer.models
+
+class NfModel {
+  val nFiscal : String? = null;
+  val nameCorporation : String? = null;
+  val cnpjAndCpf : String? = null;
+}
